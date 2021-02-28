666b3e9ee1409848546fad09d0965baafef1cfdd
roman-luzgin <luzginn@gmail.com>
2021-02-28 22:10:19 +0100

    Initial Commit

---
+++ dribbleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dribbleApp
+//
+//  Created by Roman Luzgin on 28.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dribbleApp/dribbleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dribbleAppApp.swift
+//  dribbleApp
+//
+//  Created by Roman Luzgin on 28.02.21.
+//
+
+import SwiftUI
+
+@main
+struct dribbleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
